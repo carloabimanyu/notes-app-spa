a3c07c2c4b51afb1a402d739b7ad6ba29e239864
carloabimanyu <carloabimanyu11@gmail.com>
2026-01-07 11:22:09 +0700

    feat: set up basic routing structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
-    return <h1>Hello, World!</h1>;
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<h2>This is Homepage</h2>} />
+                <Route path="/archives" element={<h2>This is Archive Page</h2>} />
+                <Route path="/notes/:id" element={<h2>This is Detail Note Page</h2>} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
