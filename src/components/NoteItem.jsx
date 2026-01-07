d58a3cf47b8f1038bc6c014286ce88e43d527b88
carloabimanyu <carloabimanyu11@gmail.com>
2026-01-07 13:53:27 +0700

    feat: make note titles clickable to detail page

--- src/components/NoteItem.jsx
+++ src/components/NoteItem.jsx
@@ -1,12 +1,16 @@
+import { Link } from "react-router-dom";
+
 function NoteItem({ note }) {
     const id = note.id;
     const title = note.title;
     const body = note.body;
     const createdAt = note.createdAt;
-    
+
     return (
         <article>
-            <h3>{title}</h3>
+            <h3>
+                <Link to={`/notes/${id}`}>{title}</Link>
+            </h3>
             <p>{createdAt}</p>
             <p>{body}</p>
         </article>
