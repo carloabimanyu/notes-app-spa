3d8ab30161b9122efff4aff5fc0aee955508c158
carloabimanyu <carloabimanyu11@gmail.com>
2026-01-07 12:10:24 +0700

    feat: add home page and connect to routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
+import HomePage from "./pages/HomePage";
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<MainLayout />}>
-                    <Route index element={<h2>This is Homepage</h2>} />
+                    <Route index element={<HomePage />} />
                     <Route path="archives" element={<h2>This is Archive Page</h2>} />
                     <Route path="notes/:id" element={<h2>This is Detail Note Page</h2>} />
                 </Route>
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,10 @@
+function HomePage() {
+    return (
+        <section>
+            <h2>Active Notes</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis assumenda quisquam sed sunt quae cum, facilis commodi quibusdam vel molestiae molestias veritatis dolorem exercitationem. Dolore, autem, dolorem dolor laboriosam suscipit praesentium omnis modi consequatur velit deleniti eos neque possimus magni illo exercitationem asperiores dicta adipisci sit aliquid architecto repellat quisquam!</p>
+        </section>
+    );
+}
+
+export default HomePage;
