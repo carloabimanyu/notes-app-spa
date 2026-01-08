14fa7682ed60eacb8e0a1d3c919c9e319da079f6
carloabimanyu <carloabimanyu11@gmail.com>
2026-01-08 15:50:17 +0700

    feat: add note detail page with route parameter

--- src/pages/NoteDetailPage.jsx
+++ src/pages/NoteDetailPage.jsx
@@ -1,14 +1,41 @@
+import React from "react";
 import { useParams } from "react-router-dom";
+import { getNoteById } from "../utils/notes-data";
 
 function NoteDetailPage() {
     const { id } = useParams();
+    return <NoteDetailPageContent noteId={id} />
+}
 
-    return (
-        <section>
-            <h2>Note Detail</h2>
-            <p>Note ID: {id}</p>
-        </section>
-    );
+class NoteDetailPageContent extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            note: null,
+        };
+    }
+
+    componentDidMount() {
+        const note = getNoteById(this.props.noteId);
+        this.setState({ note });
+    }
+
+    render() {
+        const { note } = this.state;
+
+        if (!note) {
+            return <p>Note not found</p>;
+        }
+
+        return (
+            <article>
+                <h2>{note.title}</h2>
+                <p>{note.createdAt}</p>
+                <p>{note.body}</p>
+            </article>
+        );
+    }
 }
 
 export default NoteDetailPage;
--- src/utils/notes-data.js
+++ src/utils/notes-data.js
@@ -8,4 +8,8 @@ function getArchivedNotes() {
     return notes.filter((note) => note.archived);
 }
 
-export { getActiveNotes, getArchivedNotes };
+function getNoteById(id) {
+    return notes.find((note) => note.id === id);
+}
+
+export { getActiveNotes, getArchivedNotes, getNoteById };
