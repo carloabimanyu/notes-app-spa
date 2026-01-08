ffb6a2dfc32146d4f063fc6c97b912616dcb0fc6
carloabimanyu <carloabimanyu11@gmail.com>
2026-01-08 16:07:44 +0700

    feat: make note title navigable to detail page

--- src/components/NoteItem.jsx
+++ src/components/NoteItem.jsx
@@ -1,9 +1,12 @@
+import { Link } from "react-router-dom";
 import { formatDate } from "../utils/date";
 
 function NoteItem({ note }) {
     return (
         <article>
-            <h3>{note.title}</h3>
+            <h3>
+                <Link to={`/notes/${note.id}`}>{note.title}</Link>
+            </h3>
             <p>{formatDate(note.createdAt)}</p>
             <p>{note.body}</p>
         </article>
