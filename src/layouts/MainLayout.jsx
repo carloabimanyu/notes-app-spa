52672e4816f6bfd8de6161bf7a1a1540ac61ed12
carloabimanyu <carloabimanyu11@gmail.com>
2026-01-13 11:40:33 +0700

    feat(layout): refactor main layout using MUI AppBar and Container

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,24 +1,41 @@
 import { Link, Outlet } from "react-router-dom";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
 
 function MainLayout() {
     return (
         <>
-            <header>
-                <h1>Notes App</h1>
-                <nav>
-                    <ul>
-                        <li>
-                            <Link to="/">Home</Link>
-                        </li>
-                        <li>
-                            <Link to="/archives">Archives</Link>
-                        </li>
-                    </ul>
-                </nav>
-            </header>
-            <main>
+            <AppBar position="static">
+                <Toolbar>
+                    <Typography
+                        variant="h6"
+                        component={Link}
+                        to="/"
+                        sx={{
+                            flexGrow: 1,
+                            textDecoration: "none",
+                            color: "inherit",
+                        }}
+                    >
+                        Notes App
+                    </Typography>
+
+                    <Button
+                        color="inherit"
+                        component={Link}
+                        to="/archives"
+                    >
+                        Archives
+                    </Button>
+                </Toolbar>
+            </AppBar>
+
+            <Container sx={{ mt: 4 }}>
                 <Outlet />
-            </main>
+            </Container>
         </>
     );
 }
